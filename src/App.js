394e6c6c0019ef13f6259295e24294bf11765c20
delinuxist <delinuxist@gmail.com>
2022-04-22 14:15:44 +0000

    just product import

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Products from "./components/pages/Products";
 // import {
 //   BrowserRouter,
 //   Routes,
@@ -18,6 +19,7 @@ function App() {
 
       <Contact />
       <About />
+      <Products />
     </div>
   );
 }
