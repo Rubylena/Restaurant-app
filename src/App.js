97a0f1b611bec1f7cd884bcb618cf33ed0989f06
GBENGA254 <akindejuhmikel@gmail.com>
2022-04-22 14:38:55 +0100

    Added product page and edited App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import Products from "./components/pages/Products";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-
+      <Products />
       <Contact />
       <About />
     </div>
