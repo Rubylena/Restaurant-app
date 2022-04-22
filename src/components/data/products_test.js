400dfd44953872ac5c5f08937d8d3e311a4401bc
delinuxist <delinuxist.gmail.com>
2022-04-22 14:15:03 +0000

    dummy test data for product page

---
+++ src/components/data/products_test.js
@@ -0,0 +1,39 @@
+const allProducts = [
+  {
+    id: 1,
+    name: "Nike version 1",
+    imageUrl:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+    price: 50,
+  },
+  {
+    id: 2,
+    name: "Nike version 2",
+    imageUrl:
+      "https://images.unsplash.com/photo-1642088338886-7cf60fb4d884?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: 40,
+  },
+  {
+    id: 3,
+    name: "Nike version 3",
+    imageUrl:
+      "https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: 60,
+  },
+  {
+    id: 4,
+    name: "Nike version 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1582901621692-8abda42cd03c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: 55,
+  },
+  {
+    id: 5,
+    name: "Nike version 5",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    price: 66,
+  },
+];
+
+export default allProducts;
