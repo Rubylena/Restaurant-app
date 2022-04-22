c0ab1a6a040b7ee9caf75cf5231254bc13bc8f48
delinuxist <delinuxist@gmail.com>
2022-04-22 23:15:19 +0000

    updated data

--- src/components/data/products_test.js
+++ src/components/data/products_test.js
@@ -1,38 +1,59 @@
 const allProducts = [
   {
     id: 1,
-    name: "Nike version 1",
+    name: "Pizza",
     imageUrl:
-      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+      "https://images.unsplash.com/photo-1600028068383-ea11a7a101f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 50,
   },
   {
     id: 2,
-    name: "Nike version 2",
+    name: "Burger",
     imageUrl:
-      "https://images.unsplash.com/photo-1642088338886-7cf60fb4d884?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      "https://images.unsplash.com/photo-1609167830240-fc81e9cfd9bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 40,
   },
   {
     id: 3,
-    name: "Nike version 3",
+    name: "Grilled Chicken",
     imageUrl:
-      "https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 60,
   },
   {
     id: 4,
-    name: "Nike version 4",
+    name: "Fried Chicken",
     imageUrl:
-      "https://images.unsplash.com/photo-1582901621692-8abda42cd03c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    price: 55,
+      "https://images.unsplash.com/photo-1569691899455-88464f6d3ab1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+    price: 70,
   },
   {
     id: 5,
-    name: "Nike version 5",
+    name: "Assorted Fresh Juice",
     imageUrl:
-      "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+      "https://images.unsplash.com/photo-1481671703460-040cb8a2d909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 66,
+  },
+  {
+    id: 6,
+    name: "Fried Fish",
+    imageUrl:
+      "https://images.unsplash.com/photo-1629723448738-03475a1e536d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
+    price: 45,
+  },
+  {
+    id: 7,
+    name: "Grilled Fish",
+    imageUrl:
+      "https://images.unsplash.com/photo-1587913956756-4fcf4833241d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: 77,
+  },
+  {
+    id: 8,
+    name: "Lobster",
+    imageUrl:
+      "https://images.unsplash.com/photo-1539740091271-0bd0502ebf06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: 30,
   },
 ];
 
