9e4a76d701742f9d3eb5d378784a844a8420e49e
delinuxist <delinuxist@gmail.com>
2022-04-23 02:00:35 +0000

    functional navbar for now

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Container, Navbar } from "react-bootstrap";
 
-const Navbar = () => {
+const NavbarComp = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+      <Container>
+        <Navbar.Brand>
+          {/* <img src={logo} width="40px" height="40px" /> */}
+          <NavLink className="p-2 navbar-link" to={"/"}>
+            Restaurant
+          </NavLink>
+        </Navbar.Brand>
 
-export default Navbar
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse className=" justify-content-end">
+          <div className=" d-flex flex-row ">
+            <NavLink className="p-2 navbar-link" to={"/"}>
+              Home
+            </NavLink>
+            <NavLink className="p-2 navbar-link" to={"/products"}>
+              Products
+            </NavLink>
+            <NavLink className="p-2 navbar-link" to={"/about"}>
+              About Us
+            </NavLink>
+            <NavLink className="p-2 navbar-link" to={"/contact"}>
+              Contact Us
+            </NavLink>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComp;
