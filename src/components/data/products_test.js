17333c61082f7a83dc755bd60af6360e60bc6a54
delinuxist <delinuxist@gmail.com>
2022-04-26 00:28:04 +0000

    updated data

--- src/components/data/products_test.js
+++ src/components/data/products_test.js
@@ -2,58 +2,98 @@ const allProducts = [
   {
     id: 1,
     name: "Pizza",
+    subname: "Chicken Spinach Pita Pizza",
     imageUrl:
       "https://images.unsplash.com/photo-1600028068383-ea11a7a101f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 50,
+    description:
+      "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.",
+    time: 20,
+    calories: 744,
   },
   {
     id: 2,
     name: "Burger",
+    subname: "Cheese Stuffed Burgers (Juicy Lucy)",
     imageUrl:
       "https://images.unsplash.com/photo-1609167830240-fc81e9cfd9bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 40,
+    description:
+      "Shortened in length, the term Burger is used to describe a popular sandwich made from ground meats that are formed into a patty, cooked, and placed between two halves of a bun. Although the most common Burger is made with meat, there are many alternatives that do not include meat, such as tofu or ground vegetables.",
+    time: 15,
+    calories: 500,
   },
   {
     id: 3,
     name: "Grilled Chicken",
+    subname: "Barbecue chicken",
     imageUrl:
       "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 60,
+    description:
+      "Barbecue chicken consists of chicken parts or entire chickens that are barbecued, grilled or smoked. There are many global and regional preparation techniques and cooking styles. Barbecue chicken is often seasoned or coated in a spice rub, barbecue sauce, or both.",
+    time: 25,
+    calories: 400,
   },
   {
     id: 4,
     name: "Fried Chicken",
+    subname: "The Best Buttermilk-Brined Southern Fried Chicken Recipe",
     imageUrl:
       "https://images.unsplash.com/photo-1569691899455-88464f6d3ab1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
     price: 70,
+    description:
+      "Fried chicken, also known as Southern fried chicken, is a dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried. The breading adds a crisp coating or crust to the exterior of the chicken while retaining juices in the meat.",
+    time: 35,
+    calories: 350,
   },
   {
     id: 5,
-    name: "Assorted Fresh Juice",
+    name: "Fruit Juice",
+    subname: "Assorted Fresh Juice",
     imageUrl:
       "https://images.unsplash.com/photo-1481671703460-040cb8a2d909?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 66,
+    description:
+      "Fresh juices are packed with soluble fiber, a type of fiber that supports good bacteria growth and digestive health. It has been shown to regulate blood sugar levels, lower cholesterol, and slow the transit of food through the digestive tract.",
+    time: 10,
+    calories: 100,
   },
   {
     id: 6,
     name: "Fried Fish",
+    subname: "Crispy Pan Fried Fish",
     imageUrl:
       "https://images.unsplash.com/photo-1629723448738-03475a1e536d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
     price: 45,
+    description:
+      "Fried fish is any fish or shellfish that has been prepared by frying. Often, the fish is covered in batter, egg and breadcrumbs, flour, or herbs and spices before being fried and served, often with a slice of lemon. Fish is fried in many parts of the world, and fried fish is an important food in many cuisines.",
+    time: 20,
+    calories: 200,
   },
   {
     id: 7,
     name: "Grilled Fish",
+    subname: "Grilled Fish Steaks",
     imageUrl:
       "https://images.unsplash.com/photo-1587913956756-4fcf4833241d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 77,
+    description:
+      "Naturally, this method is immensely popular and quite widespread in the maritime realm of Indonesian archipelago. Thus the grilled-barbecued fish is regarded as a classic dish of Indonesian cuisine.",
+    time: 30,
+    calories: 450,
   },
   {
     id: 8,
     name: "Lobster",
+    subname: "Red Lobster",
     imageUrl:
       "https://images.unsplash.com/photo-1539740091271-0bd0502ebf06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     price: 30,
+    description:
+      "Lobsters are a family of large marine crustaceans. Lobsters have long bodies with muscular tails, and live in crevices or burrows on the sea floor. Three of their five pairs of legs have claws, including the first pair, which are usually much larger than the others.",
+    time: 40,
+    calories: 430,
   },
 ];
 
